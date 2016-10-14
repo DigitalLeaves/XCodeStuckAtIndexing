238eea976ff0882e7767afd48619f3e079f24ba6
Ignacio Nieto <contact@digitalleaves.com>
2016-10-14 11:27:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# XCode 8 Stuck At Indexing
+
+This repository contains a sample project that will make XCode 8 get stuck at "indexing" and never be able to compile the project. It's a really simple project, just some buttons in a UIViewController and a method that initializes an array of the buttons and try to do some operations on them.
+
+Also, a method called thisOperationIsOhSoComplex() demonstrates the problems that swift 3 compiler and XCode 8 have with some operations, tagged as "too complex" when trying to build the project.
+
--- XCodeStuckAtIndexing/ViewController.swift
+++ XCodeStuckAtIndexing/ViewController.swift
@@ -8,18 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// private consts
+private let kDLNumericKeyboardNormalImage = UIImage(named: "numericKeyBackground")!
+private let kDLNumericKeyboardPressedImage = UIImage(named: "pressedNumericKeyBackground")!
 
+class ViewController: UIViewController {
+    // MARK: - outlets
+    // numbers
+    @IBOutlet weak var buttonKey0: UIButton!
+    @IBOutlet weak var buttonKey1: UIButton!
+    @IBOutlet weak var buttonKey2: UIButton!
+    @IBOutlet weak var buttonKey3: UIButton!
+    @IBOutlet weak var buttonKey4: UIButton!
+    @IBOutlet weak var buttonKey5: UIButton!
+    @IBOutlet weak var buttonKey6: UIButton!
+    @IBOutlet weak var buttonKey7: UIButton!
+    @IBOutlet weak var buttonKey8: UIButton!
+    @IBOutlet weak var buttonKey9: UIButton!
+    // backspace
+    @IBOutlet weak var buttonKeyBackspace: UIButton!
+    // symbols
+    @IBOutlet weak var buttonKeyLeftParenthesis: UIButton!
+    @IBOutlet weak var buttonKeyRightParenthesis: UIButton!
+    @IBOutlet weak var buttonKeyPoint: UIButton!
+    @IBOutlet weak var buttonKeyComma: UIButton!
+    @IBOutlet weak var buttonKeyHyphen: UIButton!
+    
+    // appearance variables
+    var normalBackgroundImage = kDLNumericKeyboardNormalImage { didSet { updateButtonsAppearance() } }
+    var pressedBackgroundImage = kDLNumericKeyboardPressedImage { didSet { updateButtonsAppearance() } }
+    var normalFontColor = UIColor.black { didSet { updateButtonsAppearance() } }
+    var pressedFontColor = UIColor.white { didSet { updateButtonsAppearance() } }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        updateButtonsAppearance()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Un-comment this method to make XCode stuck forever at "indexing"
+    fileprivate func updateButtonsAppearance() {
+        /*
+        for button in [buttonKey0, buttonKey1, buttonKey2, buttonKey3, buttonKey4, buttonKey5, buttonKey6, buttonKey7, buttonKey8, buttonKey9, buttonKeyLeftParenthesis, buttonKeyRightParenthesis, buttonKeyPoint, buttonKeyComma, buttonKeyHyphen] {
+            button?.setTitleColor(normalFontColor, for: .normal)
+            button?.setTitleColor(pressedFontColor, for: [.selected, .highlighted])
+            button?.setBackgroundImage(normalBackgroundImage, for: .normal)
+            button?.setBackgroundImage(pressedBackgroundImage, for: [.selected, .highlighted])
+        }
+         */
+    }
+    
+    // Un-comment the line below to get a weird error indicating that the expression is too complex (sigh).
+    func thisCalculationIsOhSoComplex() {
+        let inputRadius = 1.0
+        //let radius = UInt32(floor(inputRadius * 3.0 * CGFloat(sqrt(2 * M_PI)) / 4 + 0.5))
+    }
+    
 }
 
